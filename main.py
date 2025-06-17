42eb56cfd97e1c8294f7d8dff34ee27ba74a4036
Evgeniy <evgeniymalykh@gmail.com>
2025-06-17 10:11:41 +1000

    /

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from datetime import datetime
 from extensions import db
 
+
 def create_app():
     app = Flask(__name__)
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.db"
